3ff59143a177fae3b913370fae13ba5cfc6e5b01
Luiz Fernando Santana Dal Pra <113379005+luuizfernando@users.noreply.github.com>
2025-03-07 12:24:34 -0300

    fix/ excel sheet closing after inserted CIAD

---
+++ src/ANAC/anac_dates.py
@@ -0,0 +1,50 @@
+import pyautogui
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pyperclip
+import time
+
+from selenium.common.exceptions import NoSuchElementException
+
+BROWSER = webdriver.Chrome()
+BROWSER.maximize_window()
+CIADS = 0
+
+pyautogui.click(x=1205, y=1059)
+while CIADS < 3468:
+    try:
+        pyautogui.click(x=149, y=269)
+        pyautogui.click(x=252, y=198)
+        pyautogui.hotkey("ctrl", "a")
+        pyautogui.hotkey("ctrl", "c")
+        time.sleep(1)
+        CIAD = pyperclip.paste()
+        
+        pyautogui.click(x=1273, y=1054)
+        BROWSER.get("https://pergamum.anac.gov.br/")
+        time.sleep(5)
+        BROWSER.find_element(By.NAME, 'q').send_keys(CIAD)
+        pyautogui.hotkey("enter")
+        time.sleep(5)
+        try:
+            element = BROWSER.find_element(By.CLASS_NAME, 's16.negrito.preto80.hover1.link')
+            date = element.text
+            pyperclip.copy(date)
+        except NoSuchElementException:
+            error_message = "There are no ordinances for the specified CIAD"
+            pyperclip.copy(error_message)
+        
+        pyautogui.click(x=1205, y=1059)
+        time.sleep(1)
+        pyautogui.click(x=1318, y=269)
+        time.sleep(1)
+        pyautogui.click(x=252, y=198)
+        time.sleep(1)
+        pyautogui.hotkey("ctrl", "v")
+        time.sleep(1)
+        pyautogui.click(x=1910, y=972)
+
+    except NoSuchElementException as e:
+        print(f"Selenium error: {e}")
+    
+    CIADS += 1
